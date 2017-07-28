19e2564898e2dd625171a20aeb4c8bceca47a84a
lemiffe <michael.warner@showpad.com>
2017-07-28 17:49:09 +0200

    Ground styles / CS

--- index.js
+++ index.js
@@ -28,25 +28,27 @@ function inject(bot, options) {
             socket.emit('entity', bot.entity);
         });
 
-        setInterval(function() {
-            if (bot && bot.entity && bot.entity.position) {
-                let blocks = [];
-                const y = bot.entity.position.y - 1;
-                for (let x = Math.round(bot.entity.position.x) - 1; Math.round(bot.entity.position.x) + 1; x++) {
-                    for (let z = Math.round(bot.entity.position.z) - 1; Math.round(bot.entity.position.z) + 1; z++) {
-                        const x = bot.entity.position.x;
-                        const z = bot.entity.position.z;
-                        let pos = vec3(x, y, z);
-                        let block = bot.blockAt(pos);
-                        if (block && block.position) {
-                            blocks.push({type: block.type, name: block.name, x: block.position.x, z: block.position.z});
+        setTimeout(function() {
+            setInterval(function() {
+                if (bot && bot.entity && bot.entity.position) {
+                    let blocks = [];
+                    const y = bot.entity.position.y - 1;
+                    for (let x = Math.round(bot.entity.position.x) - 1; Math.round(bot.entity.position.x) + 1; x++) {
+                        for (let z = Math.round(bot.entity.position.z) - 1; Math.round(bot.entity.position.z) + 1; z++) {
+                            const x = bot.entity.position.x;
+                            const z = bot.entity.position.z;
+                            let pos = vec3(x, y, z);
+                            let block = bot.blockAt(pos);
+                            if (block && block.position) {
+                                blocks.push({type: block.type, name: block.name, x: block.position.x, z: block.position.z});
+                            }
                         }
                     }
+                    socket.emit('blocks', blocks);
+                    console.log(blocks);
                 }
-                socket.emit('blocks', blocks);
-                console.log(blocks);
-            }
-        }, 200);
+            }, 500);
+        }, 3000);
 
         bot.on('entitySpawn', function(entity) {
             socket.emit('entitySpawn', entity);
