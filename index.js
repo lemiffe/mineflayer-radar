1aab14aee35d0d087a6c10ab475e13bfcc53a3be
lemiffe <michael.warner@showpad.com>
2017-07-28 17:39:35 +0200

    Ground styles / CS

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function inject(bot, options) {
             socket.emit('entity', bot.entity);
             let blocks = [];
             const y = bot.entity.position.y;
-            //for (let x = bot.entity.position.x ; bot.entity.position.x ; x++) {
-//                for (let z = bot.entity.position.z ; bot.entity.position.z ; z++) {
+            for (let x = bot.entity.position.x - 1; bot.entity.position.x + 1; x++) {
+                for (let z = bot.entity.position.z - 1; bot.entity.position.z + 1; z++) {
                     const x = bot.entity.position.x;
                     const z = bot.entity.position.z;
                     let pos = vec3(x, y - 1, z);
@@ -37,8 +37,8 @@ function inject(bot, options) {
                     if (block && block.position) {
                         blocks.push({type: block.type, name: block.name, x: block.position.x, z: block.position.z});
                     }
-           //     }
-         //   }
+                }
+            }
             socket.emit('blocks', blocks);
         });
 
